565e2c148e97f8f29f4ae7dfc302ce959b78b359
BhaswarDutta <mail2bhaswardutta@gmail.com>
2025-09-17 22:16:36 +0530

    Update alacritty.sh with alacritty.toml config copy

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -4,12 +4,17 @@
 set -e
 
 # Download and Install JetBrainsMono Nerd Font
-
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
-
 unzip JetBrainsMono.zip -d JetBrainsMonoNerdFont
-
 mkdir -p ~/.local/share/fonts
 mv JetBrainsMonoNerdFont/* ~/.local/share/fonts/
+rm -rf JetBrainsMonoNerdFont
+rm -f JetBrainsMono.zip
 fc-cache -fv
-fc-list | grep "JetBrainsMono"
+fc-list | grep "JetBrainsMono" || echo "JetBrainsMono Nerd Font Installation Failed"
+
+
+# Copy Alacritty Config
+mkdir -p ~/.config/alacritty
+cp "templates and configs/alacritty.toml" ~/.config/alacritty/alacritty.toml
+echo "Alacritty config Installed"
