237338a667c496c49ba13ca58efc8ca5014e6b46
BhaswarDutta <mail2bhaswardutta@gmail.com>
2025-09-17 20:26:24 +0530

    Update alacritty.sh with JetBrains Mono Nerd Font Installer

--- scripts/alacritty.sh
+++ scripts/alacritty.sh
@@ -2,3 +2,14 @@
 
 # exit on failure
 set -e
+
+# Download and Install JetBrainsMono Nerd Font
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+
+unzip JetBrainsMono.zip -d JetBrainsMonoNerdFont
+
+mkdir -p ~/.local/share/fonts
+mv JetBrainsMonoNerdFont/* ~/.local/share/fonts/
+fc-cache -fv
+fc-list | grep "JetBrainsMono"
