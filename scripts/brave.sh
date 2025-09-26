a8a4365f238fc912604845c0913223f8998a3854
BhaswarDutta <mail2bhaswardutta@gmail.com>
2025-09-26 15:49:53 +0530

    Update brave.sh with brave settings copied

--- scripts/brave.sh
+++ scripts/brave.sh
@@ -5,10 +5,19 @@ set -e
 
 # Install Brave Browser
 # Install dnf-plugins-core
-sudo dnf install dnf-plugins-core
+sudo dnf install -y dnf-plugins-core
 
 # Add Brave Repo
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
 # Install Brave
 sudo dnf install -y brave-browser
+
+# Make sure Brave is closed
+pkill brave-browser || true  # ignore error if Brave isn't running
+
+# Copy Brave config
+unzip -o "$HOME/agent_pingu/templates and configs/brave-template.zip" -d "$HOME/.config/BraveSoftware/Brave-Browser/Default"
+
+# Make sure User owns the files
+chown -R $USER:$USER "$HOME/.config/BraveSoftware/Brave-Browser/Default"
