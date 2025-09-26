1dddd2a49c3765ad9626b554caba9dc7a5b6ea10
BhaswarDutta <mail2bhaswardutta@gmail.com>
2025-09-26 16:14:06 +0530

    Removed Brave Settings Copy as they were being overwritten by Brave on launch

--- scripts/brave.sh
+++ scripts/brave.sh
@@ -12,15 +12,3 @@ sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-releas
 
 # Install Brave
 sudo dnf install -y brave-browser
-
-# Make sure Brave is closed
-pkill brave-browser || true  # ignore error if Brave isn't running
-
-# Ensure Brave profile directory exists
-mkdir -p "$HOME/.config/BraveSoftware/Brave-Browser/Default"
-
-# Copy Brave config
-unzip -o "$HOME/agent_pingu/templates and configs/brave-template.zip" -d "$HOME/.config/BraveSoftware/Brave-Browser/Default"
-
-# Make sure User owns the files
-chown -R $USER:$USER "$HOME/.config/BraveSoftware/Brave-Browser/Default"
