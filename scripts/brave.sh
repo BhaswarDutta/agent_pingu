2a7b26f73fa3069e442aa3efce53fd264a106fc5
BhaswarDutta <mail2bhaswardutta@gmail.com>
2025-09-26 16:20:13 +0530

    Update brave.sh with Super + B to launch Brave

--- scripts/brave.sh
+++ scripts/brave.sh
@@ -12,3 +12,17 @@ sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-releas
 
 # Install Brave
 sudo dnf install -y brave-browser
+
+# [SUPER] + B to launch Brave with custom size/position
+# Define the path for the new custom keybinding
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings \
+"['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/brave/']"
+
+# Set the name
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/brave/ name 'Brave Custom Window'
+
+# Set the command to run
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/brave/ command 'brave-browser --window-size=1200,800 --window-position=100,100'
+
+# Set the binding (Super+B)
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/brave/ binding '<Super>b'
