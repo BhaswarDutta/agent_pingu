54907fe565f392d2f0af762aa80c31c379d2472d
BhaswarDutta <mail2bhaswardutta@gmail.com>
2025-10-09 22:51:33 +0530

    Add zed config and update zed_install.sh

--- scripts/zed_install.sh
+++ scripts/zed_install.sh
@@ -5,6 +5,5 @@ set -e
 
 curl -f https://zed.dev/install.sh | sh
 
-# Install Zed CLI
-echo 'export PATH=$HOME/.local/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+# Copy zed config and keybind
+cp -r "templates and configs/zed" ~/.config
